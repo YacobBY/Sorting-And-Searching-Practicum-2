7e323de0278d722f01ac3314303a9f29ae7fae1e
Unknown <yacob101@gmail.com>
2018-12-14 13:49:33 +0100

    paint3d description added

--- Assignment-2/src/main/java/nl/hva/ict/ss/pathfinding/tileworld/RGBColor.java
+++ Assignment-2/src/main/java/nl/hva/ict/ss/pathfinding/tileworld/RGBColor.java
@@ -19,12 +19,17 @@ public interface RGBColor {
     int RGB_CYAN = 0xff00ffff;
     int RGB_PINK = 0xffffc0cb;
 }
-//    int RGB_WHITE = ffffff;
-//    int RGB_YELLOW = ffff00;
-//    int RGB_BLUE = 0000ff;
-//    int RGB_GREY = 808080;
-//    int RGB_BLACK = 000000;
-//    int RGB_RED = ff0000;
-//    int RGB_GREEN = 00ff00;
-//    int RGB_CYAN = 00ffff;
-//    int RGB_PINK = ffc0cb
+//Read entire bottom part before opening paint3d:
+//Make maps with Paint3d, you can add all these custom colors in the color pallette with the following codes,
+//Then use the paint bucket to make a yellow base layer, make a red and green dot with the PIXEL PEN with a density of 1.
+//Black is used to draw walls(unpassable terrain), blue is for water (slower). Other colors do something too check their values.
+
+//    int RGB_WHITE =   ffffff
+//    int RGB_YELLOW =  ffff00
+//    int RGB_BLUE =    0000ff
+//    int RGB_GREY =    808080
+//    int RGB_BLACK =   000000
+//    int RGB_RED =     ff0000
+//    int RGB_GREEN =   00ff00
+//    int RGB_CYAN =    00ffff
+//    int RGB_PINK =    ffc0cb
