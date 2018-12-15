f2dc06962fac76a58a987806628a1a256d15f1fe
Unknown <yacob101@gmail.com>
2018-12-15 23:52:48 +0100

    verslag van docs als pdf erbij

--- Assignment-2/src/main/java/nl/hva/ict/ss/pathfinding/Custom/ResultPrinter.java
+++ Assignment-2/src/main/java/nl/hva/ict/ss/pathfinding/Custom/ResultPrinter.java
@@ -9,15 +9,19 @@ public class ResultPrinter {
     }
 
     public  void printStringList(ArrayList<TestResult> resultList) {
+        System.out.println("Bitmapnumber: ");
         for (TestResult test : resultList) {
             System.out.println(test.getBitmapName());
         }
+        System.out.println("Tiles");
         for (TestResult test : resultList) {
             System.out.println(test.getTiles());
         }
+        System.out.println("Length");
         for (TestResult test : resultList) {
             System.out.println(test.getLength());
         }
+        System.out.println("getCost");
         for (TestResult test : resultList) {
             System.out.println(test.getCost());
         }
